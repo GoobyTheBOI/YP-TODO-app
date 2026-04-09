8fd891c382b03b985e64bc142cb9ce1cb6a0d75e
Lentjes <luuk.lentjes@sogeti.com>
2026-04-09 12:45:56 +0200

    added simple test

---
+++ TodoService.Test/TodoService.test.cs
@@ -0,0 +1,19 @@
+﻿namespace TodoService.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Create_WithValidTitle_ReturnsConfirmationMessage()
+        {
+            // Arrange
+            TodoService service = new();
+
+            // Act
+            var result = service.Create("Buy milk");
+
+            // Assert
+            Assert.Equal("Todo created: Buy milk", result);
+        }
+
+    }
+}
---
+++ TodoService/TodoService.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoService
+{
+    public class TodoService
+    {
+        public string Create(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Title is required");
+
+            return $"Todo created: {title}";
+        }
+    }
+}
